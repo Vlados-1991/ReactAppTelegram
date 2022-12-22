55b32da81bedfe490ab1ef644f18deb781b49add
Vlad <vladlotarev@gmail.com>
2022-12-22 23:49:44 +0300

    commit4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
-
+import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
+  const {onToggleButton, tg} = useTelegram()
 
   useEffect(() => {
     tg.ready();
   }, [])
 
-  
-
   return (
     <div className="App">
-      Work
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
