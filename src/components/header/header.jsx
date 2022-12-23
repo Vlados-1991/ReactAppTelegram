a13fb73fcccbc807ce3936493e7a09414c22ff09
Vlad <vladlotarev@gmail.com>
2022-12-23 21:04:38 +0300

    commit4

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <button onClick={onClose}>Закрыть</button>
+            <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
